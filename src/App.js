c0e68958227b4dcb385e0fa44e48e225142a4111
Chimmy <jimmyeetran@gmail.com>
2023-01-29 18:19:17 +1100

    added tailwindcss

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div>
+      <h1 class="font-bold tracking-tight hover:tracking-wide">
+      Hello world!
+    </h1>
     </div>
   );
 }
