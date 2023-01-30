a3b8ec965d5d445e75b87d60e14c8493efa9a9d2
Chimmy <jimmyeetran@gmail.com>
2023-01-31 01:03:52 +1100

    fixed active css, and active nav flexboxes positioning

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,19 +7,19 @@ const Nav = () => {
         <div class='container mx-auto'>
             <div class='w-full bg-slate-300 h-[96px] backdrop-blur-2xl
             rounded-full max-w-[460px] mx-auto px-5 flex items-center justify-around text-2xl'>
-                <Link to='home' activeClass='active' smooth={true} spy={true} class='cursor-pointer w-[60px] h-[60px] flex items-center'>
+                <Link to='home' activeClass='active' smooth={true} spy={true} class='cursor-pointer w-[60px] h-[60px] flex items-center justify-around'>
                     <BiHomeAlt />
                 </Link>
-                <Link to='about' activeClass='active' smooth={true} spy={true} class='cursor-pointer w-[60px] h-[60px] flex items-center'>
+                <Link to='about' activeClass='active' smooth={true} spy={true} class='cursor-pointer w-[60px] h-[60px] flex items-center justify-around'>
                     <BiUser />
                 </Link>
-                <Link to='services' activeClass='active' smooth={true} spy={true} class='cursor-pointer w-[60px] h-[60px] flex items-center'>
+                <Link to='services' activeClass='active' smooth={true} spy={true} class='cursor-pointer w-[60px] h-[60px] flex items-center justify-around'>
                     <BsClipboardData />
                 </Link>
-                <Link to='work' activeClass='active' smooth={true} spy={true} class='cursor-pointer w-[60px] h-[60px] flex items-center'>
+                <Link to='work' activeClass='active' smooth={true} spy={true} class='cursor-pointer w-[60px] h-[60px] flex items-center justify-around'>
                     <BsBriefcase />
                 </Link>
-                <Link to='contact' activeClass='active' smooth={true} spy={true} class='cursor-pointer w-[60px] h-[60px] flex items-center'>
+                <Link to='contact' activeClass='active' smooth={true} spy={true} class='cursor-pointer w-[60px] h-[60px] flex items-center justify-around'>
                     <BsChatSquareText />
                 </Link>
 
