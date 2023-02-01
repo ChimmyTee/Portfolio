69a90f1b4b72121f51516bb9ecfacb2fb4f9eb84
Chimmy <jimmyeetran@gmail.com>
2023-02-01 23:08:25 +1100

    added fadeIn variants, started working on work

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,5 +1,25 @@
+import { motion } from 'framer-motion';
+
+import { fadeIn } from '../variants';
+
 const Work = () => {
-    return <div class='section' id='work'>Work</div>;
+    return <div class='section' id='work'>
+        <div class='container mx-auto border-2'>
+            <div>
+                <h1>My Latest Work</h1>
+                <p class='max-w-sm mb-8'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime non fugit vel aliquid facilis vero, architecto ad, dolores illo consequatur accusamus voluptates rerum earum. Consectetur magnam facere repellendus quo molestiae?</p>
+                <button class='btn'>
+                    View all projects
+                </button>
+            </div>
+
+
+            <div class='projects'>
+                
+            </div>
+
+        </div>
+    </div>;
 };
 
 export default Work;
