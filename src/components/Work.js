31f8e3d30cf2dce98f4cef15e0c67b3b89c26ae1
Chimmy <jimmyeetran@gmail.com>
2023-02-03 00:16:55 +1100

    work added animations and more functionality

--- src/components/Work.js
+++ src/components/Work.js
@@ -9,7 +9,7 @@ const Work = () => {
     return <div class='section' id='work'>
         <div class='container mx-auto border-2'>
             <div class='flex flex-col lg:flex-row gap-x-10'>
-                <div class='flex-1 flex flex-col gap-y-12'>
+                <div class='flex-1 flex flex-col gap-y-12 mb-10'>
                     <div>
                         <h1>My Latest Work</h1>
                         <p class='max-w-sm mb-8'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime non fugit vel aliquid facilis vero, architecto ad, dolores illo consequatur accusamus voluptates rerum earum. Consectetur magnam facere repellendus quo molestiae?</p>
@@ -24,9 +24,25 @@ const Work = () => {
                     </div>
                 </div>
 
-                <div>
-                    <h1>2</h1>
-                </div>
+                <motion.div
+                    variants={fadeIn('right', 0.2)}
+                    initial='hidden'
+                    whileInView={'show'}
+                    viewport={{ once: false, amount: 0.3 }}
+                    class='flex-1 flex flex-col gap-y-10'>
+                    <div class='group relative overflow-hidden border-2 border-black rounded-xl'>
+                        <div class='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                        <img class='group-hover:scale-95 transition-all duration-500' src={shoppingCartImg} alt='' />
+                        <div class='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50 text-3xl text-white'>UI/UX Design</div>
+                        <div class='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50 text-white'>Project Title</div>
+                    </div>
+                    <div class='group relative overflow-hidden border-2 border-black rounded-xl'>
+                        <div class='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                        <img class='group-hover:scale-95 transition-all duration-500' src={toDoListImg} alt='' />
+                        <div class='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50 text-3xl text-white'>UI/UX Design</div>
+                        <div class='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50 text-white'>Project Title</div>
+                    </div>
+                </motion.div>
             </div>
 
             <div class='projects'>
