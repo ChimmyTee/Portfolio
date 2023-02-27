e34dd8655ece547311f7617f9f5961d573557ca0
Chimmy <jimmyeetran@gmail.com>
2023-02-27 22:09:22 +1100

    added name to the form elements for formsubmit.co

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,9 +17,9 @@ const Contact = () => {
                     viewport={{ once: false, amount: 0.3 }}
                     action="https://formsubmit.co/jimmyeetran@gmail.com" method="POST"
                     class='flex-1 border border-zinc-300 rounded-md lg:w-4/6 lg:ml-8 px-3'>
-                    <input type='text' placeholder='Name' class='bg-transparent outline-none border-b border-zinc-300 focus:border-zinc-400 px-1 py-3 w-full' />
-                    <input type='email' placeholder='Your email' class='bg-transparent outline-none border-b border-zinc-300 focus:border-zinc-400 px-1 py-3 w-full' />
-                    <textarea type='text' placeholder='Message' class='bg-transparent outline-none border border-zinc-300 focus:border-zinc-400 px-1 py-3 mt-4 w-full' />
+                    <input type='text' name="name" placeholder='Name' class='bg-transparent outline-none border-b border-zinc-300 focus:border-zinc-400 px-1 py-3 w-full' />
+                    <input type='email' name="email" placeholder='Your email' class='bg-transparent outline-none border-b border-zinc-300 focus:border-zinc-400 px-1 py-3 w-full' />
+                    <textarea type='text' name="message" placeholder='Message' class='bg-transparent outline-none border border-zinc-300 focus:border-zinc-400 px-1 py-3 mt-4 w-full' />
                     <div class='flex justify-end'>
                         <button type="submit" class='btn m-4'>Send</button>
                     </div>
