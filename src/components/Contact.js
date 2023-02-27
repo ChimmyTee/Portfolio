22d665a170e36789c9eab106136dbfbb166acf0b
Chimmy <jimmyeetran@gmail.com>
2023-02-27 22:16:42 +1100

    replaced naked email with formsubmit string

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,7 +15,7 @@ const Contact = () => {
                     initial='hidden'
                     whileInView={'show'}
                     viewport={{ once: false, amount: 0.3 }}
-                    action="https://formsubmit.co/jimmyeetran@gmail.com" method="POST"
+                    action="https://formsubmit.co/4fb1373c183fdd5f0ea635f2d61db7d7" method="POST"
                     class='flex-1 border border-zinc-300 rounded-md lg:w-4/6 lg:ml-8 px-3'>
                     <input type='text' name="name" placeholder='Name' class='bg-transparent outline-none border-b border-zinc-300 focus:border-zinc-400 px-1 py-3 w-full' />
                     <input type='email' name="email" placeholder='Your email' class='bg-transparent outline-none border-b border-zinc-300 focus:border-zinc-400 px-1 py-3 w-full' />
